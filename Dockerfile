8a4af3ebe4f9a367e4a58dad3ec2a928ff7169a3
Ivan Rosales Rieloff <ivan.rosales.rieloff@gmail.com>
2021-07-15 02:25:21 -0400

    aws lambda test 4

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 FROM public.ecr.aws/lambda/provided:al2 as builder
 # install GIT
 WORKDIR /go/src/app
-RUN yum install -y git
-RUN git clone https://github.com/Mondongo-cl/http-rest-echo-go.git
+COPY . ./http-rest-echo-go/
+# RUN yum install -y git
+# RUN git clone https://github.com/Mondongo-cl/http-rest-echo-go.git
 RUN yum install -y golang
+
 RUN go env -w GOPROXY=direct
 WORKDIR /go/src/app/http-rest-echo-go/src
 RUN go build .
@@ -12,11 +14,15 @@ RUN go build .
 
 
 # copy artifacts to a clean image
-# FROM public.ecr.aws/lambda/go:1
-FROM public.ecr.aws/lambda/provided:al2
+FROM public.ecr.aws/lambda/go:1
+# FROM public.ecr.aws/lambda/provided:al2
 LABEL Name=http-rest-echo-go Version=0.2
+ENV GOPROXY=direct
+ENV _LAMBDA_SERVER_PORT=5001
+ENV _HANDLER=http-rest-echo-go
+ENV AWS_LAMBDA_RUNTIME_API="go.1x"
 COPY --from=builder /go/src/app/http-rest-echo-go/src/http-rest-echo-go ${LAMBDA_TASK_ROOT}
-CMD ["http-rest-echo-go"]
+CMD [ "http-rest-echo-go"]
 EXPOSE 5001
 
 ## to start container 
--- src/main.go
+++ src/main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	_ "fmt"
 	"log"
-	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -13,24 +12,22 @@ import (
 )
 
 func handleRequest(ctx context.Context, r events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-
-	log.Printf("processing vent id: %s", r.RequestContext.RequestID)
-
+	log.Printf("processing BODY: %s verb: %s path: %s", r.Body, r.HTTPMethod, r.Path)
+	defer log.Printf("event id: %s has benn processed", r.RequestContext.RequestID)
 	switch r.HTTPMethod {
-	case http.MethodGet:
-		return events.APIGatewayProxyResponse{StatusCode: 200}, nil
-	case http.MethodPost:
-		return events.APIGatewayProxyResponse{StatusCode: 201}, nil
+	case "GET":
+		return events.APIGatewayProxyResponse{StatusCode: 200, Body: "{\"Method\":\"GET\"}"}, nil
+	case "POST":
+		return events.APIGatewayProxyResponse{StatusCode: 201, Body: "{\"Method\":\"POST\"}"}, nil
 	default:
-		return events.APIGatewayProxyResponse{StatusCode: 504}, nil
-
+		return events.APIGatewayProxyResponse{StatusCode: 504, Body: "{\"Method\":\"OTHER\"}"}, nil
 	}
 }
 
 func main() {
-	println("starting hello world service...")
+	log.Println("starting hello world service...")
 	lambda.Start(handleRequest)
-
+	log.Println("stoping hello worls service...")
 	// middleware.RegisterRoutes()
 	// start()
 }
